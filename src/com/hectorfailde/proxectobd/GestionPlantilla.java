896b74d93ca7f3b2fb633b958d122aca8ad05061
hfaildeestevez <72596856+hfaildeestevez@users.noreply.github.com>
2021-05-18 20:11:29 +0200

    Add files via upload

---
+++ src/com/hectorfailde/proxectobd/GestionPlantilla.java
@@ -0,0 +1,15 @@
+
+package com.hectorfailde.proxectobd;
+
+
+public class GestionPlantilla {
+
+ 
+    public static void main(String[] args) {
+      Ventana v = new Ventana();
+      
+      v.setVisible(true);
+       
+    }
+    
+}
