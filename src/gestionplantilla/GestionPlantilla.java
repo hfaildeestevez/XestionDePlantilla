e26dc5d31526e567aedeb127ce84d3d617a43ee6
hfaildeestevez <72596856+hfaildeestevez@users.noreply.github.com>
2021-05-18 20:05:50 +0200

    Add files via upload

---
+++ src/gestionplantilla/GestionPlantilla.java
@@ -0,0 +1,15 @@
+
+package gestionplantilla;
+
+
+public class GestionPlantilla {
+
+ 
+    public static void main(String[] args) {
+      Ventana v = new Ventana();
+      
+      v.setVisible(true);
+       
+    }
+    
+}
